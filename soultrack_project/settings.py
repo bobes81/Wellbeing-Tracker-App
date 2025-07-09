dd7683b38ee6598dd3fd0526bd9ada40e29ebdeb
Ivo Kimpl <ivokimpl@Mac.powerhub>
2025-07-09 10:56:19 +0100

    Add Procfile and set ALLOWED_HOSTS for Heroku

--- soultrack_project/settings.py
+++ soultrack_project/settings.py
@@ -24,13 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6!%=26-wg35++kw3&!_do_idex!zqmv%s8sk8p7514&*-ve9g7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = [
-    'localhost',
-    '127.0.0.1',
-    'fitlog-app-ivo-6b411ba5300f.herokuapp.com'
-]
+ALLOWED_HOSTS = ['fitlog-app-ivo.herokuapp.com']
 
 
 # Application definition
