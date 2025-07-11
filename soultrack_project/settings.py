965c49ee3102fc5e6fd270ba8f6776999856a304
Ivo Kimpl <ivokimpl@Mac.powerhub>
2025-07-11 12:52:26 +0100

    Add login and logout redirect URLs to settings

--- soultrack_project/settings.py
+++ soultrack_project/settings.py
@@ -89,6 +89,9 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/accounts/login/'
+
 # Logging for debug output in Heroku logs
 if DEBUG:
     LOGGING = {
