4636d8dd94d032e5a493ece663368b9c3c927f9e
Ivo Kimpl <ivokimpl@Mac.powerhub>
2025-07-11 10:16:16 +0100

    Fix: recreated register.html template for registration view

--- soultrack_project/settings.py
+++ soultrack_project/settings.py
@@ -48,10 +48,11 @@ ROOT_URLCONF = 'soultrack_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],  # Updated to include BASE_DIR / "templates"
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
