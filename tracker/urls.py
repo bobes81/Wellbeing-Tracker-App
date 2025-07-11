14e81a8860a7a8cacdb092a8f5490e76c5995c58
Ivo Kimpl <ivokimpl@Mac.powerhub>
2025-07-11 13:46:08 +0100

    Add URL pattern for add_mood_entry view

--- tracker/urls.py
+++ tracker/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('moods/', views.mood_list, name='mood_list'),
     path('workout/create/', views.workout_create, name='workout_create'),
     path('workouts/', views.workout_list, name='workout_list'),
-    path('mood/create/', views.mood_create, name='mood_create'),  # ⬅️ přidáno
+    path('mood/create/', views.mood_create, name='mood_create'),
+    path('add-mood/', views.add_mood_entry, name='add_mood_entry'),  # Added add_mood_entry
 ]
