f2ca530980c0113b102157fcb29f50484464dd1f
Ivo Kimpl <ivokimpl@Mac.powerhub>
2025-07-12 09:30:44 +0100

    Add Heroku URL to allowed hosts

--- soultrack_project/settings.py
+++ soultrack_project/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = 'django-insecure-6!%=26-wg35++kw3&!_do_idex!zqmv%s8sk8p7514&*-ve9g7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['fitlog-app-ivo.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['fitlog-app-ivo.herokuapp.com']
 
 # Application definition
 INSTALLED_APPS = [
