bb68fe173ce0837b6b911122489cb769acede319
Ivo Kimpl <ivokimpl@Mac.powerhub>
2025-07-12 15:24:37 +0100

    FIX: Remove Mood model for working build

--- tracker/views.py
+++ tracker/views.py
@@ -1,6 +1,46 @@
-from django.shortcuts import render
-from datetime import datetime
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.utils.timezone import now
+from .models import Workout
+from .forms import WorkoutForm
 
 def home(request):
-    now = datetime.now()
-    return render(request, 'tracker/home.html', {'now': now})
+    return render(request, 'tracker/home.html', {'now': now()})
+
+@login_required
+def workout_list(request):
+    workouts = Workout.objects.filter(user=request.user).order_by('-date')
+    return render(request, 'tracker/workout_list.html', {'workouts': workouts})
+
+@login_required
+def add_workout(request):
+    if request.method == 'POST':
+        form = WorkoutForm(request.POST)
+        if form.is_valid():
+            workout = form.save(commit=False)
+            workout.user = request.user
+            workout.save()
+            return redirect('workout_list')
+    else:
+        form = WorkoutForm()
+    return render(request, 'tracker/add_workout.html', {'form': form})
+
+@login_required
+def edit_workout(request, workout_id):
+    workout = get_object_or_404(Workout, id=workout_id, user=request.user)
+    if request.method == 'POST':
+        form = WorkoutForm(request.POST, instance=workout)
+        if form.is_valid():
+            form.save()
+            return redirect('workout_list')
+    else:
+        form = WorkoutForm(instance=workout)
+    return render(request, 'tracker/edit_workout.html', {'form': form})
+
+@login_required
+def delete_workout(request, workout_id):
+    workout = get_object_or_404(Workout, id=workout_id, user=request.user)
+    if request.method == 'POST':
+        workout.delete()
+        return redirect('workout_list')
+    return render(request, 'tracker/delete_workout.html', {'workout': workout})
