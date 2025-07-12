d713707a9d9994eabf7cd62cda9147d12953fba6
Ivo Kimpl <ivokimpl@Mac.powerhub>
2025-07-12 16:43:30 +0100

    Fix: re-added missing Mood model

--- tracker/models.py
+++ tracker/models.py
@@ -20,3 +20,14 @@ class Workout(models.Model):
 
     def __str__(self):
         return f"{self.user.username} - {self.date} - {self.type}"
+
+class Mood(models.Model):
+    MOOD_CHOICES = [(i, str(i)) for i in range(1, 6)]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField()
+    mood_level = models.IntegerField(choices=MOOD_CHOICES)
+    note = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.date} - Mood: {self.mood_level}"
