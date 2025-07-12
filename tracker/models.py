13a67a11739239f1e70db0aed66873609f7c60b6
Ivo Kimpl <ivokimpl@Mac.powerhub>
2025-07-12 16:53:47 +0100

    Fix: Re-added Mood model and ran migrations

--- tracker/models.py
+++ tracker/models.py
@@ -11,7 +11,6 @@ class Workout(models.Model):
         ('Cycling', 'Cycling'),
         ('Other', 'Other'),
     ]
-
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     date = models.DateField()
     type = models.CharField(max_length=20, choices=WORKOUT_CHOICES)
