01a9d9de22da59385a95b6e39854d4f7eef90fdc
Ivo Kimpl <ivokimpl@Mac.powerhub>
2025-07-13 18:58:53 +0100

    Add custom 403 and 404 error views and handlers

--- soultrack_project/urls.py
+++ soultrack_project/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from tracker import views as tracker_views
 from django.contrib.auth import views as auth_views
+from django.conf.urls import handler403, handler404
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,5 +12,5 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
 
-handler404 = "tracker.views.custom_404"
-handler403 = "tracker.views.custom_403"
+handler404 = "tracker.views.error_404"
+handler403 = "tracker.views.error_403"
--- tracker/views.py
+++ tracker/views.py
@@ -133,3 +133,9 @@ def custom_404(request, exception):
 
 def custom_403(request, exception):
     return render(request, 'errors/403.html', status=403)
+
+def error_403(request, exception):
+    return render(request, "403.html", status=403)
+
+def error_404(request, exception):
+    return render(request, "404.html", status=404)
