80476b0ed517fa19c48cbd8581a7a35e472108db
Ivo Kimpl <ivokimpl@Mac.powerhub>
2025-07-13 13:25:33 +0100

    Add MoodAdmin to show mood_type in admin

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
-from .models import Workout, Mood  # Ensure both models are defined in models.py
+from .models import Workout, Mood
 
-admin.site.register(Workout)
-admin.site.register(Mood)
+@admin.register(Mood)
+class MoodAdmin(admin.ModelAdmin):
+    list_display = ('user', 'date', 'mood_type', 'note')  # zobrazí správná pole v seznamu
+    fields = ('user', 'mood_type', 'note')  # definuje, co se bude zobrazovat v detailu
+
+@admin.register(Workout)
+class WorkoutAdmin(admin.ModelAdmin):
+    list_display = ('user', 'date', 'type', 'duration')
